ba1dfc7339c399c6056961c3be0ac9e70ad46249
Tommy Jung <dcjung@uw.edu>
2017-11-17 00:21:49 -0800

    cl_led_dip

--- host/cl_led_dip/app.cpp
+++ host/cl_led_dip/app.cpp
@@ -1,22 +1,45 @@
 #include <iostream>
-#include "dmatester.h"
+#include "fabricmanager.h"
+#include "fpga_mgmt.h"
 using namespace std;
 
 int main(int argc, char ** argv)
 {		
-	auto dmaTester = new DMATester();
+	auto fabricManager = new FabricManager();
+	fpga_mgmt_image_info_t* info = 0;
 
 	try
 	{
-		dmaTester->TestTransferSize();
-		dmaTester->TestConcurrency();
+		// get fpga image info.
+		info = fabricManager->getImageInfo(0);
+		cout << "FPGA Image Info:" << endl;
+		printf("Vendor ID: 0x%x\r\n", info->spec.map[FPGA_APP_PF].vendor_id);
+		printf("Device ID: 0x%x\r\n", info->spec.map[FPGA_APP_PF].device_id);
+
+		// set virtual DIP to 0x0000
+		fabricManager->setvDIP(0, 0x0000);
+		cout << "setting vDIP to 0x0000" << endl;
+
+		// read virtual LED value
+		uint16_t vLED = fabricManager->getvLED(0);
+		printf("vLED = 0x%x\r\n", vLED);
+
+		// set virtual DIP to 0xbeef
+		fabricManager->setvDIP(0, 0xbeef);
+		cout << "setting vDIP to 0xbeef" << endl;
+
+		// read virtual LED value
+		vLED = fabricManager->getvLED(0);
+		printf("vLED = 0x%x\r\n", vLED);
 	}
 	catch(exception& e)
 	{
 		cout << e.what() << endl;			
 	}
 
-	delete dmaTester;
+	// make sure to free dynamically allocated memory.
+	free(info);
+	delete fabricManager;
 
 	return 0;
 }
