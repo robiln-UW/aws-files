9d70261beadfab275e280a0ea5afff2d4aed7fbb
Tommy Jung <dcjung@uw.edu>
2017-12-18 04:23:58 -0800

    host cl_dram_perf

--- host/cl_dram_perf/app.cpp
+++ host/cl_dram_perf/app.cpp
@@ -9,11 +9,13 @@
 #include <cstdlib>
 #include <chrono>
 #include <thread>
+#include <stdexcept>
 #include "fabricmanager.h"
 #include "pcihandler.h"
 #include "dmacontroller.h"
 #include "fpga_mgmt.h"
 #include "stopwatch.h"
+#include "math_helper.hpp"
 using namespace std;
 using namespace std::this_thread;
 using namespace std::chrono;
@@ -21,8 +23,12 @@ using namespace std::chrono;
 #define START_ADDR_REG_ADDR 0x500
 #define BURST_LEN_REG_ADDR 0x504
 #define WRITE_VAL_REG_ADDR 0x508
+#define RHASH_REG_ADDR 0x50c
+#define BUF_SIZE (1ULL << 34)
 
 const int SLOT_ID = 0;
+const int NUM_TRIAL = 10;
+const int BYTE_PER_BURST = 64;
 
 int main(int argc, char ** argv)
 {		
@@ -31,6 +37,9 @@ int main(int argc, char ** argv)
 	auto dmaController = new DMAController();
 	auto stopwatch = new Stopwatch();
 	fpga_mgmt_image_info_t* info = 0;
+	
+	char * buf1 = (char *) malloc(sizeof(char) * BUF_SIZE);
+	char * buf2 = (char *) malloc(sizeof(char) * BUF_SIZE);
 
 	try
 	{
@@ -46,121 +55,172 @@ int main(int argc, char ** argv)
 		// init dmaController.
 		dmaController->init(SLOT_ID);
 
-		/*
-		// init CL memory with random char.
-		size_t buf_size = 1 << 30;
-		char * buf1 = (char *) malloc(sizeof(char) * buf_size);
-		char * buf2 = (char *) malloc(sizeof(char) * buf_size);
-		
-		for (int i = 0; i < 16; i++)
-		{
-			for (int j = 0; j < buf_size; j++)
-			{
-				buf1[j] = (char) (97 + (rand() % 26));	
-			}
-	
-			dmaController->write(buf1, buf_size, 0, buf_size * i);
-			dmaController->read(buf2, buf_size, 0, buf_size * i);
-		
-			bool match = true;
-			for (int j = 0; j < buf_size; j++)
-			{
-				if (buf1[j] != buf2[j])
-				{
-					match = false;
-					printf("data mismatch. i: %d, addr: %x, buf1: %c, buf2: %c\r\n",
-						i, j, buf1[j], buf2[j]);
-				}
-			}
-
-			if (match) 
-			{
-				printf("DMA write success. i: %d\r\n", i);
-			}
-			else
-			{
-				printf("DMA write failed. i: %d\r\n", i);
-			}
-		}
-	
-		free(buf1);
-		free(buf2);
-		*/
 		// set start_addr
 		pciHandler->poke(START_ADDR_REG_ADDR, 0);
-		uint32_t start_addr = pciHandler->peek(START_ADDR_REG_ADDR);
-		printf("start_addr = %d\r\n", start_addr);
+		uint32_t start_addr_read = pciHandler->peek(START_ADDR_REG_ADDR);
+		if (start_addr_read != 0)
+		{
+			throw runtime_error("failed to write start_addr\r\n");
+		}
 
-		// read and write using mem_ctrl
+		// measure vdip, vled latency
+		double vdip_latency[NUM_TRIAL*10] = {0};
+		double vled_latency[NUM_TRIAL*10] = {0};
+		for (int i = 0; i < NUM_TRIAL*10; i++)
+		{
+			stopwatch->start();
+			fabricManager->setvDIP(SLOT_ID, 0);
+			vdip_latency[i] = stopwatch->stop();
+			
+			stopwatch->start();
+			fabricManager->getvLED(SLOT_ID);
+			vled_latency[i] = stopwatch->stop();
+		}
+	
+		double vdip_latency_avg = MathHelper::average(vdip_latency, NUM_TRIAL*10);
+		double vdip_latency_stdev = MathHelper::stdev(vdip_latency, NUM_TRIAL*10);
+		double vled_latency_avg = MathHelper::average(vled_latency, NUM_TRIAL*10);
+		double vled_latency_stdev = MathHelper::stdev(vled_latency, NUM_TRIAL*10);
+
+		printf("[vdip latency] average: %f, stdev: %f\r\n", vdip_latency_avg, vdip_latency_stdev);	
+		printf("[vled latency] average: %f, stdev: %f\r\n", vled_latency_avg, vled_latency_stdev);	
+	
+		// measure AXI-lite latency
+		double axi_lite_poke_latency[NUM_TRIAL*10] = {0};
+		double axi_lite_peek_latency[NUM_TRIAL*10] = {0};
+		for (int i = 0; i < NUM_TRIAL * 10; i++)
+		{
+			stopwatch->start();
+			pciHandler->poke(START_ADDR_REG_ADDR, 0);
+			axi_lite_poke_latency[i] = stopwatch->stop();
+
+			stopwatch->start();
+			pciHandler->peek(START_ADDR_REG_ADDR);
+			axi_lite_peek_latency[i] = stopwatch->stop();
+		}
+
+		double axi_lite_poke_latency_avg = MathHelper::average(axi_lite_poke_latency, NUM_TRIAL*10);
+		double axi_lite_poke_latency_stdev = MathHelper::stdev(axi_lite_poke_latency, NUM_TRIAL*10);
+		double axi_lite_peek_latency_avg = MathHelper::average(axi_lite_peek_latency, NUM_TRIAL*10);
+		double axi_lite_peek_latency_stdev = MathHelper::stdev(axi_lite_peek_latency, NUM_TRIAL*10);
+		
+		printf("[AXI-LITE poke latency] average: %f, stdev: %f\r\n", axi_lite_poke_latency_avg, axi_lite_poke_latency_stdev);
+		printf("[AXI-LITE peek latency] average: %f, stdev: %f\r\n", axi_lite_peek_latency_avg, axi_lite_peek_latency_stdev);
+
+		// real test
+		uint16_t vled_read = 0;	
 		uint32_t burst_len = 1;
 		for (int i = 0; i < 29; i++)
 		{
 			// set burst_len
 			pciHandler->poke(BURST_LEN_REG_ADDR, burst_len-1);
 			uint32_t burst_len_read = pciHandler->peek(BURST_LEN_REG_ADDR);
-			//printf("burst_len = %d\r\n", burst_len);
-			
-			// set write_val
-			uint32_t write_val = 0;
-			for (int j = 0; j < 8; j++)
+			if (burst_len-1 != burst_len_read)
 			{
-				write_val = (write_val << 8) + (i % 16);
+				throw runtime_error("failed to write burst_len.\r\n");
 			}
-			
-			pciHandler->poke(WRITE_VAL_REG_ADDR, write_val);
-			uint32_t write_val_read = pciHandler->peek(WRITE_VAL_REG_ADDR);
-			//printf("write_val = 0x%x\r\n", write_val_read);
 
-			// mem_ctrl write
-			stopwatch->start();
-			fabricManager->setvDIP(SLOT_ID, 0x0002);
-			uint16_t vled_read = 0x0000;
-			do {
-				vled_read = fabricManager->getvLED(SLOT_ID);
-				sleep_for(milliseconds(5));
-			} while (vled_read != 0x0002);
-			double write_latency = stopwatch->stop();	
-			printf("write,%d,%f\r\n", burst_len, write_latency);
-			fabricManager->setvDIP(SLOT_ID, 0x0000);
-			/*
-			// validate write		
-			char * buf3 = (char *) malloc(sizeof(char)*64*burst_len);
-			dmaController->read(buf3, 64 * burst_len, 0, 0);
-			bool write_valid = true;
-			for (int j = 0; j < 64 * burst_len; j++)
+			double dma_read_latency[NUM_TRIAL] = {0};
+			double dma_write_latency[NUM_TRIAL] = {0};
+			double cl_read_latency[NUM_TRIAL] = {0};
+			double cl_write_latency[NUM_TRIAL] = {0}; 
+
+			for (int j = 0; j < NUM_TRIAL; j++)
 			{
-				if (buf3[j] != (i % 16))
+				// init random char buffer
+				for (int k = 0; k < burst_len*BYTE_PER_BURST; k++)
 				{
-					printf("data mismatch. j: %d, buf3: %x, expected: %x\r\n", j, buf3[j], i % 16);
-					write_valid = false;
+					buf1[i] = (char) (97 + (rand() % 26));
+				}
+				
+				// DMA write
+				stopwatch->start();
+				dmaController->write(buf1, burst_len*BYTE_PER_BURST, 0, 0);
+				dma_write_latency[j] = stopwatch->stop();
+
+				// calculate hash
+				char rhash1[4] = {0x00,0x00,0x00,0x00};
+				for (int k = 0; k < burst_len*BYTE_PER_BURST/4; k++)
+				{
+					rhash1[0] = rhash1[0] ^ buf1[4*k];
+					rhash1[1] = rhash1[1] ^ buf1[4*k+1];
+					rhash1[2] = rhash1[2] ^ buf1[4*k+2];
+					rhash1[3] = rhash1[3] ^ buf1[4*k+3];
+				}
+
+				// CL read
+				vled_read = 0;
+				stopwatch->start();
+				fabricManager->setvDIP(SLOT_ID, 0x0001);
+				do {
+					vled_read = fabricManager->getvLED(SLOT_ID);
+				} while (vled_read != 0x0001);						
+				cl_read_latency[j] = stopwatch->stop();
+
+				uint32_t rhash2 = pciHandler->peek(RHASH_REG_ADDR);  
+				
+				printf("[rhash] expected: %x%x%x%x, actual: %x\r\n", rhash1[3], rhash1[2], rhash1[1], rhash1[0], rhash2);
+
+				// set write_val
+				uint32_t write_val = 0;
+				for (int k = 0; k < 8; k++)
+				{
+					write_val = (write_val << 8) + (j % 16);
+				}	
+			
+				pciHandler->poke(WRITE_VAL_REG_ADDR, write_val);
+				uint32_t write_val_read = pciHandler->peek(WRITE_VAL_REG_ADDR);
+				if (write_val_read != write_val)
+				{
+					throw runtime_error("failed to set write_val.\r\n");
+				}			
+
+				// CL write
+				vled_read = 0;
+				stopwatch->start();
+				fabricManager->setvDIP(SLOT_ID, 0x0002);
+				do {
+					vled_read = fabricManager->getvLED(SLOT_ID);
+				} while (vled_read != 0x0002);
+				cl_write_latency[j] = stopwatch->stop();
+
+				// DMA read
+				stopwatch->start();
+				dmaController->read(buf2, burst_len*BYTE_PER_BURST, 0, 0);	
+				dma_read_latency[j] = stopwatch->stop();
+				
+				bool valid = true;
+				for (int k = 0; k < burst_len*BYTE_PER_BURST; k++)
+				{
+					if (buf2[k] != (j % 16))
+					{
+						valid = false;
+					}
+				}
+				
+				if (!valid)
+				{
+					printf("dma data mismatch,\r\n");
 				}
 			}
 
-			if (!write_valid)
-			{
-				printf("mem_ctrl write failed. j: %d\r\n", burst_len);
-			}
-	
-			free(buf3);
-			*/
+			double dma_read_avg = MathHelper::average(dma_read_latency, NUM_TRIAL);
+			double dma_read_stdev = MathHelper::stdev(dma_read_latency, NUM_TRIAL);
+			double dma_write_avg = MathHelper::average(dma_write_latency, NUM_TRIAL);
+			double dma_write_stdev = MathHelper::stdev(dma_write_latency, NUM_TRIAL);
+				
+			double cl_read_avg = MathHelper::average(cl_read_latency, NUM_TRIAL);
+			double cl_read_stdev = MathHelper::stdev(cl_read_latency, NUM_TRIAL);
+			double cl_write_avg = MathHelper::average(cl_write_latency, NUM_TRIAL);
+			double cl_write_stdev = MathHelper::stdev(cl_write_latency, NUM_TRIAL);
+				
+			printf("dma,read,%d,%f,%f\r\n", burst_len, dma_read_avg, dma_read_stdev);
+			printf("dma,write,%d,%f,%f\r\n", burst_len, dma_write_avg, dma_write_stdev);
+			printf("cl,read,%d,%f,%f\r\n", burst_len, cl_read_avg, cl_read_stdev);
+			printf("cl,write,%d,%f,%f\r\n", burst_len, cl_write_avg, cl_write_stdev);
 
-			/*
-			// mem_ctrl read
-			stopwatch->start();
-			fabricManager->setvDIP(SLOT_ID, 0x0001);
-			vled_read = 0x0000;
-
-			do {
-				vled_read = fabricManager->getvLED(SLOT_ID);
-				sleep_for(milliseconds(5));
-			} while (vled_read != 0x0001);
-			double read_latency = stopwatch->stop();
-			printf("read,%d,%f\r\n", burst_len, read_latency);
-			fabricManager->setvDIP(SLOT_ID, 0x0000);
-			*/
 			burst_len *= 2;
 		}
-
 
 	}
 	catch(exception& e)
@@ -170,10 +230,12 @@ int main(int argc, char ** argv)
 
 	// make sure to free dynamically allocated memory.
 	free(info);
+	free(buf1);
+	free(buf2);
 	delete fabricManager;
 	delete pciHandler;
 	delete dmaController;
-
+	delete stopwatch;
 
 	return 0;
 }
